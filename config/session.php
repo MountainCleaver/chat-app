b49c3d1fe6797137a96d425eafc842df175295d5
Julius Romero <juliusromero009988@gmail.com>
2025-06-20 21:27:09 +0800

    check if user is logged in

--- config/session.php
+++ config/session.php
@@ -1,5 +1,39 @@
 <?php 
 
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+
     require_once '../../config/database.php';
 
-    
+    $response = [
+        'status'  => false,
+        'message' => '',
+        'data'    => null
+    ];
+
+    if(!$conn){
+        http_response_code(500);
+        $response['status'] = false;
+        $response['message'] = 'Database connection failed.';
+        $response['data'] = null;
+        echo json_encode($response);
+        exit;
+    }
+
+    if($_SERVER['REQUEST_METHOD'] ==- 'POST'){
+        if(empty($_SESSION['user_id']) || empty($_SESSION['user_email']) || empty($_SESSION['user'])){
+            $response['message'] = 'user not logged in';
+            echo json_encode($response);
+            exit;
+        } else {
+            $username = $_SESSION['user'];
+            $response['status']  = true;
+            $response['message'] = "Welcome {$username}";
+            $response['data']    = [
+                'id'       => $_SESSION['user_id'],
+                'email'    => $_SESSION['user_email'],
+                'username' => $_SESSION['user']
+            ];
+            echo json_encode($response);
+        }
+    }
