0e6bb788f1cabcd129d4674a868e6dcd6df14428
Julius Romero <juliusromero009988@gmail.com>
2025-06-26 22:49:45 +0800

    will put target user into session variable

---
+++ apis/home/set_contact.php
@@ -0,0 +1,35 @@
+<?php 
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Method: POST');
+    header('Access-Control-Allow-Headers: Content-Type');
+
+    require_once '../../config/database.php';
+
+    $response = [   
+        'status' => false,
+        'message' => '',
+        'to_message' => null
+    ];
+
+    try {
+        $raw_data = file_get_contents('php://input');
+        $data = json_decode($raw_data, true);
+
+        if (!isset($data['user_id'])) {
+            throw new Exception('No target user provided.');
+        }
+
+        $target_user_id = (int)$data['user_id'];
+
+        $_SESSION['to_message'] = $target_user_id;
+
+        $response['status'] = true;
+        $response['message'] = 'Target user set successfully.';
+        $response['to_message'] = $_SESSION['to_message'];
+
+    } catch (Exception $e) {
+        http_response_code(400);
+        $response['message'] = $e->getMessage();
+    }
+
+echo json_encode($response);
